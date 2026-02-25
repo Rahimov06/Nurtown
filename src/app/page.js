2ddebc47e9b8022f47afa4759368cef68040a7bf
Rahimov Muhammadkarim <rahimovmuhammadkarim737@gmail.com>
2026-02-25 11:54:45 +0500

    fix code

--- src/app/page.js
+++ src/app/page.js
@@ -1,43 +1,12 @@
-"use client"
-
-import Image from "next/image";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
-import { TextField } from "@mui/material";
-import Link from "next/link";
-import CardSwiper from "./component/cardswiper";
-import { useState } from "react";
-import AboutCompany from "./component/aboutcompony";
-import Tasksection from "./component/tasksection";
-import Project from "./component/project";
-import Contact from "./component/contact";
+import CardSwiper from "./component/home/cardswiper";
+import AboutCompany from "./component/home/aboutcompony";
+import Tasksection from "./component/home/tasksection";
+import Project from "./component/home/project";
+import Contact from "./component/home/contact";
 
 export default function Home() {
-  const [showModal, setShowModal] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    phone: "",
-    email: "",
-    product: "",
-    message: ""
-  });
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-
-    setShowModal(true);
-    setFormData({
-      name: "",
-      phone: "",
-      email: "",
-      product: "",
-      message: ""
-    });
-  };
+ 
+ 
 
   return (
     <>
