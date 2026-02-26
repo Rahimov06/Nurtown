9342e8faff184eb79181b809739bfe5d47e0d489
Rahimov Muhammadkarim <rahimovmuhammadkarim737@gmail.com>
2026-02-26 11:58:14 +0500

    lang

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./component/layout/navbar";
 import Footer from "./component/layout/footer";
+import "../i18n";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
